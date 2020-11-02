bb002be6724586795e34196435d65df984b69f3f
386083520 <386083520@qq.com>
2020-11-03 07:36:34 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
